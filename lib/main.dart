13bed068e2ce9b8738fce0d6336269c8a31f4a9d
AllahDitta37 <75024095+AllahDitta37@users.noreply.github.com>
2021-01-09 11:57:05 +0500

    some error fix

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,19 @@ import 'dart:async';
 
 void main() => runApp(SplashScreen());
 
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AuthService().handleAuth(),
+    );
+  }
+}
+
+
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
